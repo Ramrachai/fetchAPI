56422cc07d54096c23846fbf9b36322af6d3fc45
Ramrachai <ramrachaim@gmail.com>
2020-02-01 20:17:10 +0600

    update fetch.js

--- fetch.js
+++ fetch.js
@@ -1,6 +1,6 @@
 console.log("hello api");
 
-fetch("https://crossorigin.me/https://www.metaweather.com/api/location/44418/")
+fetch("https://www.metaweather.com/api/location/44418/")
   .then(result => {
     console.log(result);
     return result.json();
